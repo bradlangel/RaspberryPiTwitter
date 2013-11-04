ff15a743d7ef04b850661fd0ea9980bd5f107c36
omnipotentnova <bradlangel@casketJane.local>
2013-11-04 18:29:11 -0500

    took away keyInfo

--- start.py
+++ start.py
@@ -3,13 +3,7 @@
 
 from feedrenderer import *
 from twitterrepository import *
-
-# You must register your own application and fill in the following fields.
-# https://dev.twitter.com/docs/auth/tokens-devtwittercom
-consumerKey = 'y9h2wiSBqvAg8xwhu67KA'
-consumerSecret = 'aQSis1UMnwZjTWPNxt5zglyuZZKqiNeDT85c4O2vTE'
-apiKey = '39705383-1tAeEMpeWe1DXTWVOaYaWuHR3CCy7cQMCJ5GCJMub'
-apiSecret = 'emv9SrFa71OVP8videz8sbsvOqLldYri6hwgqtUb4YLv0'
+from APIProperties import *
 
 renderer = FeedRenderer(2, 2)
 
